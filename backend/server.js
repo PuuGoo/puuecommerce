5c5159aa37d4c2ef1641b3c771589d7e851ec78f
PuuGoo <ndphu1997@gmail.com>
2023-11-01 14:57:45 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server is working on http://localhost:${process.env.PORT}`);
-});
+const server = http.createServer(process.env.PORT || 3000);
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+// });
 
 // Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
